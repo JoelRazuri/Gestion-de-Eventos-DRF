aa08cf7b2a4b9b4b8b578387cef28065fffbd43c
Joel Razuri <jrazuri98@hotmail.com>
2024-06-18 20:34:37 -0300

    Event Registration Comment Rating models

--- events/models.py
+++ events/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import CustomUser
+from .validations import *
 
-# Create your models here.
+
+class Event(models.Model):
+    tilte = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to='event_images/')
+    date = models.DateTimeField(validators=[validator_is_valid_date], blank=False, null=False)
+    capacity = models.IntegerField(validators=[validator_capacity], blank=False, null=False)
+    organizer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, validators=[validator_is_organizer])
+
+
+class Registration(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    registered_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    comment = models.TextField(blank=False, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+class Rating(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[validator_rating], blank=False, null=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+
