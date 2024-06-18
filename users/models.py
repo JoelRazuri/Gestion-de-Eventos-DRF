5e2cf75445ad4ea8f9424c9b5c5c4465f9ca7e80
Joel Razuri <jrazuri98@hotmail.com>
2024-06-18 19:55:40 -0300

    CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+def validator_role(value):
+    if 1 > value > 3:
+        raise ValueError('El rol solo puede ser 1, 2 o 3')
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, blank=False, null=False, unique=True, verbose_name='Nombre de usuario')
+    name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Nombres')
+    last_name = models.CharField(max_length=100, blank=True, null=True, verbose_name='Apellidos')
+    email = models.EmailField(unique=True, verbose_name='Email')
+    password = models.CharField(max_length=100, verbose_name='Contraseña')
+    role = models.IntegerField(validators=[validator_role], verbose_name='Rol')
+
+
+    def __str__(self):
+        return self.username  
+    
+
+    
