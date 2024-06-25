d170e25c4e4798bf4e54a3ad8e817109220a45b9
Joel Razuri <jrazuri98@hotmail.com>
2024-06-24 22:38:13 -0300

    Events classViews first steps

--- events/views.py
+++ events/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from .serializers import EventSerializer
+from .models import Event
+from users.models import CustomUser
+from rest_framework import status
+from rest_framework.views import  APIView
+from rest_framework.response import  Response
+from django.http import Http404
 
-# Create your views here.
+
+
+class EventCreateListView(APIView):
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EventDetailUpdateDeleteView(APIView):
+    def get_object(self, pk):
+        try:
+            return Event.objects.get(pk=pk)
+        except Event.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        event = self.get_object(pk)
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        event = self.get_object(pk)
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
