3e15ad4fef95b7166d850ba28f85366327b40f09
Joel Razuri <jrazuri98@hotmail.com>
2024-06-25 19:53:29 -0300

    modifying model structure

--- events/models.py
+++ events/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from users.models import CustomUser
-from .validations import *
 
 
 class Event(models.Model):
     tilte = models.CharField(max_length=200, blank=False, null=False)
     description = models.TextField(blank=False, null=False)
-    image = models.ImageField(upload_to='event_images/')
-    date = models.DateTimeField(validators=[validator_is_valid_date], blank=False, null=False)
-    capacity = models.IntegerField(validators=[validator_capacity], blank=False, null=False)
-    organizer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, validators=[validator_is_organizer])
+    image = models.ImageField(upload_to='event_images/', null=True, blank=True)
+    date = models.DateTimeField(blank=False, null=False)
+    capacity = models.IntegerField(blank=False, null=False)
+    organizer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.tilte
@@ -31,6 +30,6 @@ class Comment(models.Model):
 class Rating(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    rating = models.IntegerField(validators=[validator_rating], blank=False, null=False)
+    rating = models.IntegerField(blank=False, null=False)
     created_date = models.DateTimeField(auto_now_add=True)
 
