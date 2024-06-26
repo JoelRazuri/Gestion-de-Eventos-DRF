fe2e5ad39e4dc0c81a207663abd9ae6420992f98
Joel Razuri <jrazuri98@hotmail.com>
2024-06-25 21:49:34 -0300

    move url-format to general urls

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,4 @@
 from .views import EventCreateListView, EventDetailUpdateDeleteView
-from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 
 urlpatterns = [
@@ -7,5 +6,3 @@ urlpatterns = [
     path('events/<int:pk>', EventDetailUpdateDeleteView.as_view()),
 ]
 
-
-urlpatterns = format_suffix_patterns(urlpatterns)
