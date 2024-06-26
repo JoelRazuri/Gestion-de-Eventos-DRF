10a7418f5b071e15cd169f3b10d85ea03bdc7ead
Joel Razuri <jrazuri98@hotmail.com>
2024-06-25 21:47:55 -0300

    create CustomUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import CustomUser
-
+from events.models import Event
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    events = serializers.PrimaryKeyRelatedField(many=True, queryset=Event.objects.all())
+    
     class Meta:
         model = CustomUser
         fields = '__all__'
