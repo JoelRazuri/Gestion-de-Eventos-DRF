96bed84641eb3c41aad3f84dc49c9706d9ae2b29
Joel Razuri <jrazuri98@hotmail.com>
2024-06-26 23:36:49 -0300

    add url for view ListUserRegistration

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
-from .views import UserListView, UserDetailView
+from .views import UserListView, UserDetailView, ListUserRegistrationsView
 from django.urls import path
 
 urlpatterns = [
     path('users/', UserListView.as_view()),
     path('users/<int:pk>', UserDetailView.as_view()),
+    path('users/registrations/', ListUserRegistrationsView.as_view())
 ]
