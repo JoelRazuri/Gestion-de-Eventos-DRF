85e609ca4e63c49330a1843ba82391545ea2fc92
Joel Razuri <jrazuri98@hotmail.com>
2024-06-26 23:25:34 -0300

    Registration urls

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,13 @@
-from .views import EventCreateListView, EventDetailUpdateDeleteView
 from django.urls import path
+from .views import (
+    EventCreateListView, 
+    EventDetailUpdateDeleteView,
+    RegisterForEventView
+    )
 
 urlpatterns = [
     path('events/', EventCreateListView.as_view()),
     path('events/<int:pk>', EventDetailUpdateDeleteView.as_view()),
+    path('events/<int:event_id>/register/', RegisterForEventView.as_view()),
 ]
 
