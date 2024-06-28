261cf18577ac33e56ceeb4516703116f89401fae
Joel Razuri <jrazuri98@hotmail.com>
2024-06-27 23:16:33 -0300

    create two news urls for profile views and users views

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,18 @@
-from .views import UserListView, UserDetailView, ListUserRegistrationsView
+from .views import (
+    UserListView, 
+    UserDetailView, 
+    UserListRegistrationsView, 
+    ProfileListRegistrationsView,
+    ProfileView
+)
 from django.urls import path
 
 urlpatterns = [
+    # Users urls for administration
     path('users/', UserListView.as_view()),
     path('users/<int:pk>', UserDetailView.as_view()),
-    path('users/registrations/', ListUserRegistrationsView.as_view())
+    path('users/<int:user_id>/registrations/', UserListRegistrationsView.as_view()),
+    # Profile user urls
+    path('profile/', ProfileView.as_view()),
+    path('profile/registrations/', ProfileListRegistrationsView.as_view()),
 ]
