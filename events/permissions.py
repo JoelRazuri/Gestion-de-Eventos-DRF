f801cca7440cbbf3febedc76e1a786787395d86f
Joel Razuri <jrazuri98@hotmail.com>
2024-06-27 23:43:07 -0300

    fix error

--- events/permissions.py
+++ events/permissions.py
@@ -4,5 +4,12 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return obj.owner == request.user
+        return obj.organizer == request.user
+    
+
+class IsOrganizerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.role == 1 or request.user.role == 2
     
