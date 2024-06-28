27c16ca8c25552db58d39ae6bebbdfb5530b6d76
Joel Razuri <jrazuri98@hotmail.com>
2024-06-27 23:17:05 -0300

    create news views for profile users n user views for administrations

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from events.serializers import RegistrationSerializer
 from events.models import Registration
 from .serializers import CustomUserSerializer
@@ -7,13 +8,14 @@ from rest_framework.views import  APIView
 from rest_framework.response import  Response
 from django.http import Http404
 
-
+# Users Views for administration
+# faltan crear permisos para quien utiliza estas vistas, solo deben ser administradores (role=1)
 class UserListView(APIView):
     def get(self, request, format=None):
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+  
 
 class UserDetailView(APIView):
     def get_object(self, pk):
@@ -28,8 +30,27 @@ class UserDetailView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class ListUserRegistrationsView(APIView):
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+class UserListRegistrationsView(APIView):
+    def get(self, request, user_id):
+        user = get_object_or_404(CustomUser, id=user_id)
+        registrations = Registration.objects.filter(user=user)
+        serializer = RegistrationSerializer(registrations, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+# Profile Views for user
+class ProfileView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        profile = CustomUser.objects.get(id=request.user.id)
+        serializer = CustomUserSerializer(profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProfileListRegistrationsView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         registrations = Registration.objects.filter(user=request.user)
