70f6f1e9ba33594e36c10db9b1cd64c7d0ed6948
Joel Razuri <jrazuri98@hotmail.com>
2024-07-04 20:58:31 -0300

    create new permission for owner to comment

--- events/permissions.py
+++ events/permissions.py
@@ -1,12 +1,19 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwnerEventOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.organizer == request.user
-    
+
+   
+class IsOwnerCommentOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user == request.user
+
 
 class IsOrganizerOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
