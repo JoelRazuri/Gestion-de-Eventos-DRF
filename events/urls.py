6624c07aca3393c9b2e8862534ccc29c01067ce1
Joel Razuri <jrazuri98@hotmail.com>
2024-07-04 20:06:40 -0300

    add Comments urls

--- events/urls.py
+++ events/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from .views import (
     EventCreateListView, 
     EventDetailUpdateDeleteView,
-    RegisterForEventView
+    RegisterEventView,
+    CommentsEventCreateListView
     )
 
 urlpatterns = [
     path('events/', EventCreateListView.as_view()),
-    path('events/<int:pk>/', EventDetailUpdateDeleteView.as_view()),
-    path('events/<int:pk>/register/', RegisterForEventView.as_view()),
+    path('events/<int:event_id>/', EventDetailUpdateDeleteView.as_view()),
+    path('events/<int:event_id>/register/', RegisterEventView.as_view()),
+    path('events/<int:event_id>/comments/', CommentsEventCreateListView.as_view()),
+    path('events/<int:event_id>/comments/<int:comment_id>/', CommentsEventCreateListView.as_view()),
+    
+    # path('events/<int:event_id>/rating/', RegisterEventView.as_view())
 ]
 
