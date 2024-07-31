4214cecba72c585d83a9d5cf6118961321e49e36
Joel Razuri <jrazuri98@hotmail.com>
2024-07-31 20:20:47 -0300

    create CustomUserListSerializer and create function to encript password for CustomUserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,18 @@ from .models import CustomUser
 from events.models import Event
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'name', 'last_name', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        user = CustomUser(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+    
+
+class CustomUserListSerializer(serializers.ModelSerializer):
     events = serializers.PrimaryKeyRelatedField(many=True, queryset=Event.objects.all())
     
     class Meta:
