93a15a37aadbccb70aa96d41340faa13784c378f
Joel Razuri <jrazuri98@hotmail.com>
2024-07-31 22:39:47 -0300

    first part LoginUserView

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from .serializers import CustomUserSerializer, CustomUserListSerializer
 from .models import CustomUser
 from rest_framework import status, permissions
 from rest_framework.views import  APIView
+from rest_framework.authtoken.views import ObtainAuthToken 
 from rest_framework.response import  Response
 from django.http import Http404
 
@@ -67,6 +68,9 @@ class RegisterUserView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class LoginUserView(APIView):
+class LoginUserView(ObtainAuthToken):
     def post(self, request, format=None):
-        pass
+        login_serializer = self.serializer_class(data=request.data, context={'request':request})
+        if login_serializer.is_valid():
+            return Response()
+        return Response(login_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
