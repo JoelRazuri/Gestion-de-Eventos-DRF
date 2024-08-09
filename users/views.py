91be260549972a3189ddc59a3bf9e6102836a4a3
Joel Razuri <jrazuri98@hotmail.com>
2024-08-08 21:32:02 -0300

    delete views for users administrations - refactoring

--- users/views.py
+++ users/views.py
@@ -11,36 +11,6 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import  Response
 from django.http import Http404
 
-# Users Views for administration
-# faltan crear permisos para quien utiliza estas vistas, solo deben ser administradores (role=1)
-class UserListView(APIView):
-    def get(self, request, format=None):
-        users = CustomUser.objects.all()
-        serializer = CustomUserListSerializer(users, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-  
-
-class UserDetailView(APIView):
-    def get_object(self, user_id):
-        try:
-            return CustomUser.objects.get(id=user_id)
-        except CustomUser.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, user_id, format=None):
-        user = self.get_object(user_id)
-        serializer = CustomUserListSerializer(user)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class UserListRegistrationsView(APIView):
-    def get(self, request, user_id):
-        user = get_object_or_404(CustomUser, id=user_id)
-        registrations = Registration.objects.filter(user=user)
-        serializer = RegistrationSerializer(registrations, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 
 # Profile Views for user
 class ProfileView(APIView):
