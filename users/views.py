28cfaf5ee8cea55a85103bf3317fb09b9dda72ce
Joel Razuri <jrazuri98@hotmail.com>
2024-08-08 21:30:15 -0300

    create LogoutUserView n import TokenAuthentication

--- users/views.py
+++ users/views.py
@@ -3,10 +3,11 @@ from events.serializers import RegistrationSerializer
 from events.models import Registration
 from .serializers import CustomUserSerializer, CustomUserListSerializer, CustomUserTokenSerializer
 from .models import CustomUser
-from rest_framework import status, permissions
+from rest_framework import status
 from rest_framework.views import  APIView
 from rest_framework.authtoken.views import ObtainAuthToken 
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import  Response
 from django.http import Http404
 
@@ -43,7 +44,7 @@ class UserListRegistrationsView(APIView):
 
 # Profile Views for user
 class ProfileView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
     def get(self, request):
         profile = CustomUser.objects.get(id=request.user.id)
@@ -52,7 +53,7 @@ class ProfileView(APIView):
 
 
 class ProfileListRegistrationsView(APIView):
-    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [TokenAuthentication]
 
     def get(self, request):
         registrations = Registration.objects.filter(user=request.user)
@@ -68,6 +69,7 @@ class RegisterUserView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LoginUserView(ObtainAuthToken):
     def post(self, request, format=None):
@@ -89,3 +91,12 @@ class LoginUserView(ObtainAuthToken):
                 return Response({'message_error': 'Ya ha iniciado sesión'}, status=status.HTTP_409_CONFLICT)
             return Response({'message': 'El usuario no esta activo'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(login_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LogoutUserView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request, format=None):
+        token = Token.objects.get(user=request.user)
+        token.delete()
+        return Response({'message': 'Sesión cerrada'}, status=status.HTTP_200_OK)
