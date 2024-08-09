59fbf7d076249f1bc94d7311b306a7304ea37e32
Joel Razuri <jrazuri98@hotmail.com>
2024-08-08 21:31:35 -0300

    delete urls for user administration - refactoring

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,4 @@
 from .views import (
-    UserListView, 
-    UserDetailView, 
-    UserListRegistrationsView, 
     ProfileListRegistrationsView,
     ProfileView,
     RegisterUserView,
@@ -11,10 +8,6 @@ from .views import (
 from django.urls import path
 
 urlpatterns = [
-    # Users urls for administration
-    path('users/', UserListView.as_view()),
-    path('users/<int:user_id>', UserDetailView.as_view()),
-    path('users/<int:user_id>/registrations/', UserListRegistrationsView.as_view()),
     # Profile user urls
     path('profile/', ProfileView.as_view()),
     path('profile/registrations/', ProfileListRegistrationsView.as_view()),
