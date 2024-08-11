b731da945d47bd227f7657576a9a2907e1df3a7a
Joel Razuri <jrazuri98@hotmail.com>
2024-08-11 16:15:11 -0300

    documentating app event

--- events/admin.py
+++ events/admin.py
@@ -1,7 +1,7 @@
 from .models import Event, Registration, Comment, Rating
 from django.contrib import admin
 
-
+# Register models Event, Registration, Comment and Rating in administration of Django
 admin.site.register(Event)
 admin.site.register(Registration)
 admin.site.register(Comment)
--- events/urls.py
+++ events/urls.py
@@ -9,11 +9,15 @@ from .views import (
     )
 
 urlpatterns = [
+    # Events urls
     path('events/', EventCreateListView.as_view()),
     path('events/<int:event_id>/', EventDetailUpdateDeleteView.as_view()),
+    # Register event url
     path('events/<int:event_id>/register/', RegisterEventView.as_view()),
+    # Comments events urls
     path('events/<int:event_id>/comments/', CommentsEventCreateListView.as_view()),
     path('events/<int:event_id>/comments/<int:comment_id>/', CommentsEventDetailUpdateDeleteView.as_view()),
+    # Rating envent url
     path('events/<int:event_id>/ratings/', RatingsEventView.as_view())
 ]
 
