47f3e77fd80ce28732304cbc3d4b3cc3bae54819
Joel Razuri <jrazuri98@hotmail.com>
2025-02-08 20:01:41 -0300

    urls para drf-spectacular

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('events.urls')),
     path('api/', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
+
+    # Genera el esquema OpenAPI
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Documentación interactiva (Swagger UI)
+    path('api/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
