207ad8ecc61611057bca23076c18ade274a4b35a
Joel Razuri <jrazuri98@hotmail.com>
2025-02-11 21:32:13 -0300

    correcion print

--- users/permissions.py
+++ users/permissions.py
@@ -3,5 +3,4 @@ from rest_framework import permissions
 
 class IsAdministrator(permissions.BasePermission):
     def has_permission(self, request, view):
-        print(f"Usuario autenticado: {request.user}")
         return request.user.is_authenticated and request.user.role == 1
